702cb87013dadef3d42002ee987c71b78f286435
Karl Gordon <kgordon@stsci.edu>
2025-04-01 14:47:14 -0400

    adding exclude regions and other things

--- utils/fit_model.py
+++ utils/fit_model.py
@@ -5,6 +5,7 @@ import time
 import numpy as np
 import matplotlib.pyplot as plt
 import astropy.units as u
+from astropy.table import QTable
 
 from measure_extinction.stardata import StarData
 from measure_extinction.extdata import ExtData
@@ -19,17 +20,29 @@ os.environ["OMP_NUM_THREADS"] = "1"
 def fit_model_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument("starname", help="Name of star")
-    parser.add_argument("--path", help="Path to star data", default="/home/kgordon/Python/extstar_data/DAT_files/")
+    parser.add_argument(
+        "--path",
+        help="Path to star data",
+        default="/home/kgordon/Python/extstar_data/DAT_files/",
+    )
     parser.add_argument(
         "--modtype",
         help="Pick the type of model grid",
         choices=["obstars", "whitedwarfs"],
         default="obsstars",
     )
-    parser.add_argument("--wind", help="add IR (lambda > 1 um) wind emission model", action="store_true")
-    parser.add_argument("--modpath", help="path to the model files", default="/home/kgordon/Python/extstar_data/Models/")
     parser.add_argument(
-        "--modstr", help="Alternative model string for grid (expert)", default="tlusty_z100"
+        "--wind", help="add IR (lambda > 1 um) wind emission model", action="store_true"
+    )
+    parser.add_argument(
+        "--modpath",
+        help="path to the model files",
+        default="/home/kgordon/Python/extstar_data/Models/",
+    )
+    parser.add_argument(
+        "--modstr",
+        help="Alternative model string for grid (expert)",
+        default="tlusty_z100",
     )
     parser.add_argument(
         "--picmodel",
@@ -38,12 +51,14 @@ def fit_model_parser():
     )
     parser.add_argument(
         "--Av_init", help="initial A(V) for fitting", default=1.0, type=float
-    )    
+    )
     parser.add_argument("--mcmc", help="run EMCEE MCMC fitting", action="store_true")
     parser.add_argument(
         "--mcmc_nsteps", help="number of MCMC steps", default=1000, type=int
     )
-    parser.add_argument("--showfit", help="display the best fit model plot", action="store_true")
+    parser.add_argument(
+        "--showfit", help="display the best fit model plot", action="store_true"
+    )
     return parser
 
 
@@ -64,10 +79,18 @@ def main():
     fstarname = f"{args.starname}.dat"
     reddened_star = StarData(fstarname, path=f"{args.path}", only_bands=only_bands)
 
+    # read in the foreground HI values
+    htab = QTable.read("data/hi_foregrounds_m31_id.fits")
+    gvals = args.starname.split("_")[1] == htab["star"]
+    forehi = htab["nhi"][gvals][0] * 1e20
+    forehi_unc = htab["nhi_err"][gvals][0] * 1e20
+
     # remove low S/N STIS data - affected by systematics
     sn_cut = 1.5
     snr = reddened_star.data["STIS"].fluxes / reddened_star.data["STIS"].uncs
-    bvals = np.logical_and(snr < sn_cut, reddened_star.data["STIS"].waves > 0.17 * u.micron)
+    bvals = np.logical_and(
+        snr < sn_cut, reddened_star.data["STIS"].waves > 0.17 * u.micron
+    )
     reddened_star.data["STIS"].npts[bvals] = 0
     reddened_star.data["STIS"].fluxes[bvals] = 0
 
@@ -115,16 +138,53 @@ def main():
 
     if "Teff" in reddened_star.model_params.keys():
         memod.logTeff.value = np.log10(float(reddened_star.model_params["Teff"]))
-        memod.logTeff.fixed = True
+        if "Teff_unc" in reddened_star.model_params.keys():
+            memod.logTeff.prior = (
+                memod.logTeff.value,
+                float(reddened_star.model_params["Teff_unc"])
+                / (float(reddened_star.model_params["Teff"]) * np.log(10.0)),
+            )
+        else:
+            memod.logTeff.fixed = True
     if "logg" in reddened_star.model_params.keys():
         memod.logg.value = float(reddened_star.model_params["logg"])
-        memod.logg.fixed = True
+        if "logg_unc" in reddened_star.model_params.keys():
+            memod.logg.prior = (
+                memod.logg.value,
+                float(reddened_star.model_params["logg_unc"]),
+            )
+        else:
+            memod.logg.fixed = True
     if "Z" in reddened_star.model_params.keys():
         memod.logZ.value = np.log10(float(reddened_star.model_params["Z"]))
         memod.logZ.fixed = True
     if "velocity" in reddened_star.model_params.keys():
         memod.velocity.value = float(reddened_star.model_params["velocity"])
         memod.velocity.fixed = True
+
+    # add exclude regions
+    exreg = {
+        "e1": [0.17, 0.2],
+        "e2": [0.1701, 0.1702],
+        "e3": [0.1701, 0.1702],
+        "e4": [0.17, 0.2],
+        "e5": [0.18, 0.2],
+        "e6": [0.183, 0.2],
+        "e7": [0.17, 0.19],
+        "e8": [0.162, 0.2],
+        "e9": [0.1701, 0.1702],
+        "e12": [0.165, 0.205],
+        "e13": [0.165, 0.205],  # [.137, .143, .165, .205]
+        "e14": [0.165, 0.21],
+        "e15": [0.183, 0.2],
+        "e17": [0.185, 0.19],
+        "e18": [0.18, 0.21],
+        "e22": [0.17, 0.19],
+        "e24": [0.18, 0.2],
+    }
+
+    snum = (args.starname.split("_"))[1]
+    memod.add_exclude_region(np.flip(1.0 / (np.array(exreg[snum]) * u.micron)))
 
     memod.fit_weights(reddened_star)
 
@@ -141,9 +201,23 @@ def main():
 
     memod.Av.value = args.Av_init
 
+    # setup HI values
+    memod.logHI_exgal.value = np.log10(1.61e21 * memod.Av.value)
+    memod.logHI_exgal.fixed = False
+    memod.vel_exgal.value = -10.0
+    memod.vel_exgal.fixed = False
+
+    memod.logHI_MW.value = np.log10(forehi)
+    memod.fore_Av.value = forehi / (8.3e21 / 3.1)
+    memod.logHI_MW.fixed = True
+    memod.vel_MW.fixed = True
+
+    # set velocities to non-zero to help fitting
+    memod.velocity.value = 10.0
+
     # for M31
     memod.logZ.fixed = True
-    #memod.velocity.fixed = True
+    # memod.velocity.fixed = True
     memod.logTeff.fixed = False
     memod.logg.fixed = False
     memod.velocity.fixed = False
@@ -156,6 +230,9 @@ def main():
     # memod.logHI_MW.fixed = True
 
     memod.set_initial_norm(reddened_star, modinfo)
+
+    # dictonary for fit parameter tables
+    fit_params = {}
 
     print("initial parameters")
     memod.pprint_parameters()
@@ -172,6 +249,9 @@ def main():
 
     print("best parameters")
     fitmod.pprint_parameters()
+    fit_params["MIN"] = fitmod.save_parameters()
+
+    dust_columns = {"AV": (fitmod.Av.value, 0.0), "RV": (fitmod.Rv.value, 0.0)}
 
     fitmod.plot(reddened_star, modinfo, resid_range=resid_range, lyaplot=lyaplot)
     plt.savefig(f"{outname}_minimizer.pdf")
@@ -193,6 +273,12 @@ def main():
 
         print("p50 parameters")
         fitmod2.pprint_parameters()
+        fit_params["MCMC"] = fitmod2.save_parameters()
+
+        dust_columns = {
+            "AV": (fitmod2.Av.value, fitmod2.Av.unc),
+            "RV": (fitmod2.Rv.value, fitmod2.Rv.unc),
+        }
 
         fitmod2.plot(reddened_star, modinfo, resid_range=resid_range, lyaplot=lyaplot)
         plt.savefig(f"{outname}_mcmc.pdf")
@@ -220,9 +306,8 @@ def main():
     # get the reddened star data again to have all the possible spectra
     reddened_star_full = StarData(fstarname, path=f"{args.path}", only_bands=only_bands)
     extdata.calc_elx(reddened_star_full, modsed_stardata, rel_band="ACS_F475W")
-    col_info = {"av": fitmod.Av.value, "rv": fitmod.Rv.value}
-    extdata.columns = {"AV": (fitmod.Av.value, 0.0), "RV": (fitmod.Rv.value, 0.0)}
-    extdata.save(f"{outname.replace("figs", "exts")}_ext.fits", column_info=col_info)
+    extdata.columns = dust_columns
+    extdata.save(f"{outname.replace("figs", "exts")}_ext.fits", fit_params=fit_params)
 
     if args.showfit:
         fitmod.plot(reddened_star, modinfo, resid_range=resid_range, lyaplot=lyaplot)
